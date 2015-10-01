4964eb32b8124b1d17e5a94b9965b1a7e20934e6
OOTANI,Kenji <ootani@virgintech.co.jp>
2015-10-01 16:40:43 +0000

    Deleteing function added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,13 @@
 class MessagesController < ApplicationController
   
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def edit
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   def update
